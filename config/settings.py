45902413f1bf0406d799d4587992bd75b1d04048
Reznov Ammar <reznov110@gmail.com>
2021-11-02 22:47:58 +0600

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,11 @@
+import base64
+
+ASSIGNEE = '5e8a666c2c0eff0b8fa24694'
+AUTH_USER = 'reznov110@gmail.com'
+AUTH_TOKEN = 'quR5NX7ZL2PvXkMxGeV1CEC6'
+MAIN_URL = 'https://jira-tt-123.atlassian.net/rest/api/2'
+
+REQUESTS_HEADER = {
+  'Content-Type': 'application/json',
+  'X-Atlassian-Token': 'no-check',
+}
