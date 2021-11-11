aefc7ab820eef5c2b03dd0255b00b7da0f8aa69a
Reznov Ammar <reznov110@gmail.com>
2021-11-11 15:50:15 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 AUTH_USER = 'reznov110@gmail.com'
 AUTH_TOKEN = ''
-MAIN_URL = 'https://jira-tt-123.atlassian.net/rest/api/2'
+MAIN_URL = 'https://jira-tt-123.atlassian.net'
 
 REQUESTS_HEADER = {
   'Content-Type': 'application/json',
